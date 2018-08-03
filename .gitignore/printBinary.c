22c87e6457bba93192f607091fbd3ce44d9ae70d
Adnan <adnankocak_68@hotmail.com>
2018-08-03 09:38:33 +0300

    Update printBinary.c

--- .gitignore/printBinary.c
+++ .gitignore/printBinary.c
@@ -2,38 +2,34 @@
 
 int start(int number)
 {
-	int i;	
-	for(i = 31;i >= 0;i--)
-		if(((number >> i) & 0x1) == 1)
-			return i;
-		
-	return 0;		
+    int i;	
+    for(i = 31;i >= 0;i--)
+	if(((number >> i) & 0x1) == 1)
+	   break;		
+    return i;		
 }
 
 void printBinary(int number)
 {
-	int i;
-	printf("\nthe number in binary is: ");
+    int i;
+    printf("\nthe number in binary is: ");
 	
-	for(i = start(number);i >= 0;i--)
-		printf("%d",((number >> i) & 0x1));
+    for(i = start(number);i >= 0;i--)
+	printf("%d",((number >> i) & 0x1));
 }
 
 int main()
 {
-	int inputNumber;
+    int inputNumber;
 	
-	printf("\t\t32 Bit Converter (decimal --> binary)\n\n");
-
-    // reads a decimal number from the user.
+    printf("\t\t32 Bit Converter (decimal --> binary)\n\n");
     printf("\nenter a integer number: ");
     scanf("%d",&inputNumber);
-
 	
     printBinary(inputNumber);
     
     fflush(stdin);
     getchar();
-    
-	return 0;
+	
+    return 0;
 }
